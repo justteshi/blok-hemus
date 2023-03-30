372f46a471f1c38c33816afbd43999053d51e0ae
thrsitov <thristov@abrites.com>
2023-03-30 15:53:03 +0300

    Add icon

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,23 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import img from '../public/sgrada_1.svg'
 
 export default function Home() {
+    console.log(img)
     return (
         <div className={styles.container}>
+            
             <Head>
                 <title>Blok Hemus Docs</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main>
+                <div className='logo-wrapper'>
+                    <div className='logo' style={{backgroundImage: `url(${img.src})`}}></div>
+                </div>
                 <h1 className={styles.title}>
-                    Блок Хемус Документи
+                    Блок <span className='hemus-title' >Хемус</span> <br></br> Документи
                 </h1>
 
                 <p className={styles.description}>
